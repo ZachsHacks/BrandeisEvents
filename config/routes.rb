7a5b7f76203ed75ad4cfc1d1501a519096fcb5c3
Zachary Weiss <coolshale@gmail.com>
2017-02-19 20:09:54 -0600

    Connected to Event page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   get '/', to: "welcome#index"
-
+	get '/events/new', to: "event#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "welcome#index"
 end
