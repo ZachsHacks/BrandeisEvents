87e5beea81b0258b417a9ee38ba6a40722f327c4
Aaron Gold <aharonhillel@brandeis.edu>
2017-03-01 15:54:20 -0500

    Updated Header to use partial as well as updated events page to be 3 wide, sidebar 2 wide

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,31 @@
 module EventsHelper
+#
+# def initialize
+#   @events = events
+# end
+#
+# # def list_events
+# #
+# #   <div class="col-md-4 portfolio-item">
+# #       <a href="#">
+# #           <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+# #       </a>
+# #       <h4>
+# #           <a href="#">Project Name</a>
+# #       </h4>
+# #       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+# #       <div class="row">
+# #           <div class="col-xs-6">
+# #               <div>
+# #                   <span class="glyphicon glyphicon-time" id="icon-home" class="col-md-2"></span>Feb. 9. 5:00PM</div>
+# #           </div>
+# #           <div class="col-xs-6">
+# #               <div class="text-right">
+# #                   <span class="glyphicon glyphicon-map-marker" id="icon-location" class="text-right"></span>SCC</div>
+# #           </div>
+# #       </div>
+# #   </div>
+# #
+# # end
+#
 end
