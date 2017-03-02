9d89f4632238c046a7f560bf42797566004b82d8
Zachary Weiss <coolshale@gmail.com>
2017-03-02 14:25:17 -0500

    can create tags for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
 	has_many :event_tags
-	validates :name , presence: true
-	validates :description , presence: true
-	validates :location , presence: true
-	validates :start , presence: true
+	# validates :name , presence: true
+	# validates :description , presence: true
+	# validates :location , presence: true
 end
