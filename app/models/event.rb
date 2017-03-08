d26c88011aff7031f285f8a1406a75bd4bf55d2f
Zachary Weiss <coolshale@gmail.com>
2017-03-07 22:32:32 -0500

    event tests added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
 	has_many :event_tags
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :description, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	def setup
+		@event = Event.new(name: "My Event", description: "This event is awesome")
+	end
+
+	test "should be valid" do
+		assert @event.valid?
+	end
+
+	test "name should be present" do
+		@event.name = ""
+		assert_not @event.valid?
+	end
+
+	test "description should be present" do
+		@event.description = "     "
+		assert_not @event.valid?
+	end
+
 end
