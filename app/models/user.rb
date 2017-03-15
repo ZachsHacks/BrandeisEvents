3bb0c9a357ad355d395e0dd16c3788ae6a70d20f
Ari Carr <acarr@brandeis.edu>
2017-03-15 16:17:14 -0400

    Byebyebug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
     class << self
       def from_omniauth(auth_hash)
         user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
-        byebug
+
         user.first_name = auth_hash['info']['first_name']
-        #byebug
         user.last_name = auth_hash['info']['last_name']
-        #byebug
         user.name = user.first_name + ' ' + user.last_name
-        #byebug
         user.email = auth_hash['extra']['raw_info']['email']
         user.location = auth_hash['info']['location']
         user.image_url = auth_hash['info']['image']
