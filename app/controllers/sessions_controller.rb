e992cfb76d05ed0787812089544c9d65814d3057
Ari Carr <acarr@brandeis.edu>
2017-04-03 10:02:58 -0400

    Un-hardcoded authentication URL and made login redirect to current user's page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.name}!"
+      redirect_to current_user
     rescue
       flash[:warning] = 'There was an error while trying to authenticate you...'
+      redirect_to root_path
     end
-    redirect_to root_path
   end
 
   def destroy
---
+++ app/helpers/auth_helper.rb
@@ -0,0 +1,7 @@
+module AuthHelper
+
+  def get_url
+    '/auth/google'
+  end
+
+end
