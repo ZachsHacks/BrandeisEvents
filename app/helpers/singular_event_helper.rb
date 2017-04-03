b839f82e36015071c91b2fafebadcfcd8ef926ad
Zachary Weiss <coolshale@gmail.com>
2017-04-02 22:24:09 -0400

    fixed tests

--- app/helpers/singular_event_helper.rb
+++ app/helpers/singular_event_helper.rb
@@ -32,37 +32,37 @@ module SingularEventHelper
 	end
 
 
-def sidebar_individual_date(event)
-     <<-eos
-   <h4>Dates</h4>
-  <div id="column_results">
-    <p>#{event.start.strftime("%a %B %d %I:%M ") } </p>
-  </div>
-  eos
-end
-def sidebar_individual_locations(event)
-  <<-eos
-  <h4>Locations</h4>
-    <div id="column_results">
-    <p>#{event.location}</p>
-    </div>
-  eos
-end
-def sidebar_individual_price(event)
-s =""
-  s<<"<h4>Price</h4>"
-  s<<"<div id='column_results'>"
-s<<"<p>$#{event.price}</p>"
-s<<"  </div>"
+	def sidebar_individual_date(event)
+		<<-eos
+		<h4>Dates</h4>
+		<div id="column_results">
+		<p>#{event.start.strftime("%a %B %d %I:%M ") } </p>
+		</div>
+		eos
+	end
+	def sidebar_individual_locations(event)
+		<<-eos
+		<h4>Locations</h4>
+		<div id="column_results">
+		<p>#{event.location}</p>
+		</div>
+		eos
+	end
+	def sidebar_individual_price(event)
+		s =""
+		s<<"<h4>Price</h4>"
+		s<<"<div id='column_results'>"
+		s<<"<p>$#{event.price}</p>"
+		s<<"  </div>"
 
-@event.tags.each do |a|
-s<<"   <h4>Tags</h4>"
-s<<"   <div id='column_results'>"
-s<<"   <p>#{a.tags}</p>"
-s<<"   </div>"
-end
-s.html_safe
-end
+		@event.tags.each do |a|
+			s<<"   <h4>Tags</h4>"
+			s<<"   <div id='column_results'>"
+			s<<"   <p>#{a.name}</p>"
+			s<<"   </div>"
+		end
+		s.html_safe
+	end
 
 
 
