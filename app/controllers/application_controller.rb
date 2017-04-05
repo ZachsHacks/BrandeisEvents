793386581604b4ba8947e7c1cae5b9d7b222521c
hdstusnick <hdstusnick@gmail.com>
2017-04-05 14:40:19 -0400

    added rails_auth and cancancan but authorization is not yet set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :store_location
+  include CanCan::ControllerAdditions
 
   private
 
