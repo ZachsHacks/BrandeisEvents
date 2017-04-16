9360a18541ac78251a7d8e7fe2abf341f475ae1c
Ari Carr <acarr@brandeis.edu>
2017-04-16 14:05:12 -0400

    Users can't view other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    if @user != current_user
+        redirect_to root_path
+    end
   end
 
   def creation_form
