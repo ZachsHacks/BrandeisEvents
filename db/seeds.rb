0a29fe2f13e50847075bbe48b68dd12cfa323f35
Zachary Weiss <coolshale@gmail.com>
2017-04-17 22:30:47 -0400

    started working on tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
 require 'faker'
 require_relative 'brandeis_event_parser'
+require_relative 'tag_dictionary'
 
 @user_count = User.count
 
+#trumba data for brandeis events
 @data = BrandeisEventParser.new.get_data
+
+#dictionary for creating tags (related words)
+@dictionary = TagDictionary.new.dictionary
 
 def create_events
 	@locations = Location.all.pluck(:name)
@@ -14,7 +19,8 @@ def create_events
 		date_time = Time.parse(line["published"].to_s)
 		relavent_website = line["gc:weblink"]
 		image_id = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRCV8cQhEbPEz0yF0piMIseNgxSAKW7FOImmw7LoWS3wniHvGZW"
-		Event.find_or_create_by(name: title, description: description, location: location, start: date_time, user: User.first, image_id: image_id)
+		e = Event.find_or_create_by(name: title, description: description, location: location, start: date_time, user: User.first, image_id: image_id)
+		create_tags(e)
 	end
 
 end
@@ -52,23 +58,20 @@ end
 def create_host
 	User.find_or_create_by(uid: "calendar", provider: "google", first_name: "BrandeisEvents",   email: "calendar@brandeis.edu", can_host: true, is_admin: false)
 end
-def create_tags
-	tag = ["Sports", "Politics", "Music", "Cooking", "Medicine"]
-	tag.each do |t|
-		Tag.find_or_create_by(name: t)
-	end
 
-end
-
-def create_tags
-	tags = ["religious", "clubs", "food", "academic", "sports"]
+def create_tags(event)
+	description = event.description
+	word_list = description.split.map { |w| w.downcase } || event.name.split.map { |w| w.downcase }
+	word_list = word_list.uniq
+	tags = @dictionary.keys.select { |word| word_list.include?(word) } || @dictionary.values.select { |word| word_list.include?(word) }
 	tags.each do |t|
-		Tag.find_or_create_by(name: t)
+		event.tags.create(name: t)
 	end
+	byebug
 end
 
 def create_locations
-	File.open("db/locations.txt").each do |line|
+	File.open("db/seeds/locations.txt").each do |line|
 		line = line.gsub("\t", "")
 		line = line.gsub("\n", "")
 		Location.find_or_create_by(name: line)
@@ -82,7 +85,6 @@ def debug_events
 end
 
 create_host
-create_tags
 create_locations
 create_events
 debug_events
---
+++ db/tag_dictionary.rb
@@ -0,0 +1,57 @@
+require "related_word"
+require "byebug"
+
+class TagDictionary
+
+	attr_reader :dictionary
+
+	def initialize
+		@word_finder = RelatedWord.new
+		if File.file?("db/seeds/dictionary_json.txt")
+			@dictionary = JSON.parse(File.open("db/seeds/dictionary_json.txt").read)
+			@dictionary = {} if @dictionary.empty?
+		else
+			@dictionary = {}
+		end
+
+		generate_dictionary if @dictionary == {}
+
+	end
+
+	private
+
+	def generate_dictionary
+		File.open("db/seeds/tags.txt").each do |tag|
+			tag = tag.gsub("\t", "")
+			tag = tag.gsub("\n", "")
+			self.populate(tag)
+		end
+	end
+
+	def populate(word)
+		word_list = @word_finder.find(word)
+		word_list = word_list.map { |line| line[:word].downcase}
+		@dictionary[word.downcase] = word_list.uniq
+	end
+
+end
+
+# Wordnik.configure do |config|
+# 	config.api_key = '5c43d251812f5d116dd5d0df80c0ab2fb760fea7b03af5557'
+# end
+#
+#
+# puts Wordnik.word.get_related('sports', :type => 'hypernym', :use_canonical => true)
+# #
+# td = TagDictionary.new
+# #
+# #
+# File.open("seeds/tags.txt").each do |tag|
+# 	tag = tag.gsub("\t", "")
+# 	tag = tag.gsub("\n", "")
+# 	td.populate(tag)
+# end
+#
+# output = File.open( "seeds/dictionary_json.txt","w" )
+# output.write(td.dictionary.to_json)
+# output.close
