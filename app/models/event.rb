59e5e3a2330335b8ae05f096a83e47c11e5ae292
Aaron Gold <aharonhillel@brandeis.edu>
2017-04-24 10:31:24 -0400

    Fixed bad merge --- thanks github for inserting random strings

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,38 +26,38 @@ class Event < ApplicationRecord
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :event_image, content_type: /\Aimage\/.*\Z/
 
-    def self.search(params)
-        if params
-            if params[:event]
-                name = params[:event].downcase unless params[:event].blank?
-                    # THis is present so that the all event search bar can search both title and loction
-                location = params[:event].downcase unless params[:location] == 'all'
-          	end
-            if params[:location]
-                location = params[:location].downcase unless params[:location] == 'all'
-          	end
-            # unless params[:date].blank?
-            #     date_only = params[:date][/\A\d+/]
-            #     date_searched = Date.parse(date_only)
-            # end
-            # date = Date.strptime(params[:date], '%m/%d/%Y %I:%M %p') unless params[:date].blank?
-            # self.where(['lower(name) LIKE ? OR lower(location) LIKE ? OR start LIKE ?', "%#{name}%", "%#{location}%", "%#{date}%"]).order('id DESC')
-            if location == name
-                where(['lower(name) LIKE ? OR lower(description) LIKE ? OR lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
+		def self.search(params)
+	        if params
+	            if params[:event]
+	                name = params[:event].downcase unless params[:event].blank?
+	                    # THis is present so that the all event search bar can search both title and loction
+	                location = params[:event].downcase unless params[:location] == 'all'
+	          	end
+	            if params[:location]
+	                location = params[:location].downcase unless params[:location] == 'all'
+	          	end
+	            # unless params[:date].blank?
+	            #     date_only = params[:date][/\A\d+/]
+	            #     date_searched = Date.parse(date_only)
+	            # end
+	            # date = Date.strptime(params[:date], '%m/%d/%Y %I:%M %p') unless params[:date].blank?
+	            # self.where(['lower(name) LIKE ? OR lower(location) LIKE ? OR start LIKE ?', "%#{name}%", "%#{location}%", "%#{date}%"]).order('id DESC')
+	            if location == name
+	                where(['lower(name) LIKE ? OR lower(description) LIKE ? OR lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
 
-            else location != name
-							if (params[:event].present? && params[:location].present?)
-                 where(['lower(name) LIKE ? AND lower(description) LIKE ? AND lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
-							 elsif (!params[:event].present? && params[:location].present?)
-								 where(['lower(location) LIKE ?', "%#{location}%"]).order('id DESC')
-							 else
-								 where(['lower(name) LIKE ? OR lower(description) LIKE ? OR lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
+	            else location != name
+								if (params[:event].present? && params[:location].present?)
+	                 where(['lower(name) LIKE ? AND lower(description) LIKE ? AND lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
+								 elsif (!params[:event].present? && params[:location].present?)
+									 where(['lower(location) LIKE ?', "%#{location}%"]).order('id DESC')
+								 else
+									 where(['lower(name) LIKE ? OR lower(description) LIKE ? OR lower(location) LIKE ?', "%#{name}%", "%#{name}%", "%#{location}%"]).order('id ASC')
+				end
 			end
-		end
-        else
-            order('id ASC')
-        end
-    end
+	        else
+	            order('id ASC')
+	        end
+	    end
 
     def self.all_current_locations
         all.pluck(:location)
--- config/routes.rb
+++ config/routes.rb
@@ -17,13 +17,10 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get '/search', to: 'events#search'
   post '/get_events', to: 'users#get_events', as: 'get_events'
-<<<<<<< HEAD
   get 'users/:id/new_account', to: 'users#creation_form', as: 'new_account'
-=======
+
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
->>>>>>> 7f77ddb5d1ca73f16451a379a4593110c9d0d624
-
   post '/rsvp', to: 'rsvps#rsvp', as: 'rsvp'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
