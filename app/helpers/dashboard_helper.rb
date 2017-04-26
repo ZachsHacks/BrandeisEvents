a2cb9ca4fb9a9540cfc56e6ac504d9476e4c192f
Aaron Gold <aharonhillel@brandeis.edu>
2017-04-26 14:57:17 -0400

    Fixed top events to actually work but home page is very slow

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,11 +1,11 @@
 module DashboardHelper
 
     def tab_titles
-		['RSVP\'d events', 'Interested Events', 'Events For You']
+		['RSVP\'d events', 'Interested Events', 'Events For You', 'Your Profile']
 	end
 
 	def section_titles
-		['Events you\'re going to', 'Events you\'re interested in', 'Recommended events for you']
+		['Events you\'re going to', 'Events you\'re interested in', 'Recommended events for you', 'Update Your Account']
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425174154) do
+ActiveRecord::Schema.define(version: 20170426165807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170425174154) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
