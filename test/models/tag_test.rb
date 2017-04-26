2e18abf54a759ac93dc161dcb0837204946d9aaa
Aaron Gold <aharonhillel@brandeis.edu>
2017-04-25 23:00:19 -0400

    Added tests for additing and deleting a tag for an event

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
-
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+		@manual = true
+		@host = users(:host)#User.create(uid: "zweiss1", first_name: "Example", last_name: "User", email: "user@example.com", provider: "google", can_host: true, is_admin: false)
+		@guest = users(:two) #User.create(uid: "acarr", first_name: "Example1", last_name: "User1", email: "user1@example.com", provider: "google", can_host: false, is_admin: false)
+		@event = events(:one)#Event.new(user_id: @host.id, name: "My Event", description: "This event is awesome. This event is awesome. This event is awesome. This event is awesome.")
+		@event.user_id = @host.id
+	end
+
+
+  test "Can add tag to an event" do
+    assert_difference('@event.tags.count', 1) do
+      # byebug
+      @event.tags.create(name: "politics")
+    end
+  end
+
+  test "Can delete tag to an event" do
+      @event.tags.create(name: "politics")
+    assert_difference('@event.tags.count', -1) do
+      # byebug
+      @event.tags.last.destroy
+    end
+  end
+
 end
