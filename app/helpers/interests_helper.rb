f792b8b1f36bceb4c1265e4737429e1b00c27361
Aaron Gold <aharonhillel@brandeis.edu>
2017-04-26 18:52:09 -0400

    All new Users new page!!!!

--- app/helpers/interests_helper.rb
+++ app/helpers/interests_helper.rb
@@ -2,9 +2,9 @@ module InterestsHelper
 
     def tag_button_color(tag)
       if current_user.interests.pluck(:tag_id).include?(tag.id)
-        'btn btn-primary'
+        'btn btn-primary btn-interests'
       else
-        'btn btn-default'
+        'btn btn-default btn-interests'
       end
     end
 
