9f940a69cf4007c4cb43a870f632086e583e0d3f
Ari Carr <acarr@brandeis.edu>
2017-04-28 10:49:43 -0400

    No more request attributes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.omniauth :saml,
       idp_cert_fingerprint: 'DD:92:3D:F7:0F:6E:86:6E:DC:EE:E9:3B:25:84:A7:72:C9:40:6B:29',
       issuer: 'campusnow.herokuapp.com',
-    #   request_attributes: [
+      request_attributes: [],
     #     { :name => 'uid', :friendly_name => 'uid' },
     #     { :name => 'mail;eppn', :name_format => 'name_identifier_format:urn:oasis:names:tc:SAML:2.0:nameid-format:transient', :friendly_name => 'mail' },
     #     { :name => 'givenName', :name_format => 'name_identifier_format:urn:oasis:names:tc:SAML:2.0:nameid-format:transient', :friendly_name => 'givenName' },
