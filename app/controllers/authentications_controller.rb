1219800b13688b1253a9428ca9be42c99baaa712
Ari Carr <acarr@brandeis.edu>
2017-04-29 20:42:33 -0400

    Kill authentication controller redirect

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,8 @@
 class AuthenticationsController < Devise::OmniauthCallbacksController
     skip_before_action :verify_authenticity_token
-    
+
     def saml
         puts '😃 We made it!'
-        redirect_to root_path
     end
 
 end
