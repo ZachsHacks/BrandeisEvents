25b32a80eb4dcb303ae612f82989ac1f16641356
Ari Carr <acarr@brandeis.edu>
2017-04-29 21:20:52 -0400

    AAAAAAHHHHHHH

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,7 +4,10 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
     def saml
         begin
             hash = request.env['omniauth.auth']
-            puts hash.keys
+            puts hash.keys.each do |k|
+                puts "hash[#{k}]: #{hash[k]}"
+
+            end
             # byebug
             # puts "Email =  #{hash.attributes['urn:oid:0.9.2342.19200300.100.1.3']}"
             # puts "UID =  #{hash.attributes['urn:oid:0.9.2342.19200300.100.1.1']}"
