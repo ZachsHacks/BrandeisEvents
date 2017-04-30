00dc1e6089a02a0a9d1743205c6c03096ac4e9f4
Ari Carr <acarr@brandeis.edu>
2017-04-29 22:29:11 -0400

    New user page

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,10 +4,15 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
     def saml
         begin
             hash = request.env['omniauth.auth']
+            new_user = User.find_by(email: hash['urn:oid:0.9.2342.19200300.100.1.3'][0]).nil?
           @user = User.from_omniauth(hash)
           session[:user_id] = @user.id
           flash[:success] = "Welcome, #{@user.name}!"
-          redirect_back_or @user
+          if new_user
+ +			redirect_to new_account_path(@user.id)
+ +		  else
+ +			redirect_back_or @user
+ +		  end
         rescue
           flash[:warning] = 'There was an error while trying to authenticate you...'
           puts 'You\'re screwed...'
