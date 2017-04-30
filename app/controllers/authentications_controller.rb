1c32c503585f2decd9319ad6349cbd1f08500a17
Ari Carr <acarr@brandeis.edu>
2017-04-29 20:46:39 -0400

    Actual authentication method

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,6 +3,17 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
 
     def saml
         puts '😃 We made it!'
+        begin
+            puts request.env['omniauth.auth']
+            # redirect_to root_path
+        #   @user = User.from_omniauth(request.env['omniauth.auth'])
+        #   session[:user_id] = @user.id
+        #   flash[:success] = "Welcome, #{@user.name}!"
+        #   redirect_back_or @user
+        rescue
+          flash[:warning] = 'There was an error while trying to authenticate you...'
+          redirect_to root_path
+        end
     end
 
 end
