0cdb1abd543ac1c2bc0346395568dc1b3dc8ce18
Aaron Gold <aharonhillel@brandeis.edu>
2017-04-30 08:20:18 -0400

    Playing with HTML5 Geolocation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 		has_many :interests
 		has_many :tags, through: :interests
 
+		validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+
     class << self
       def from_omniauth(auth_hash)
         user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
