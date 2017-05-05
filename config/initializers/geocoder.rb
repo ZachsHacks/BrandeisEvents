77bcafefc046e4f4ba596404e1a61ce95c9fbc67
Aaron Gold <aharonhillel@brandeis.edu>
2017-05-05 10:07:18 -0400

    Customer Geocoder file

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,12 @@
 Geocoder.configure(
   # Geocoding options
-  timeout: 15                 # geocoding service timeout (secs)
-  # lookup: :google,            # name of geocoding service (symbol)
+  timeout: 15,                # geocoding service timeout (secs)
+  lookup: :google,            # name of geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: ENV['GOOGLE_MAPS_API_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
@@ -15,7 +15,7 @@ Geocoder.configure(
   # supports SocketError and Timeout::Error
   # always_raise: [],
 
-  # Calculation options
-  units: :mi,                 # :km for kilometers or :mi for miles
-  distances: :linear          # :spherical or :linear
+  # # Calculation options
+  # units: :mi,                 # :km for kilometers or :mi for miles
+  # distances: :linear          # :spherical or :linear
 )
