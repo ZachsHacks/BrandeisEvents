1d93c1ec4081dfbe2302d17893abe0c34dbe7203
Zachary Weiss <coolshale@gmail.com>
2017-05-07 11:54:19 -0400

    fixed bot

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,12 +1,13 @@
 require "facebook/messenger"
+require_relative '../models/event'
 include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN_Facebook"])
 # message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
 # message.sender      # => { 'id' => '1008372609250235' }
 # message.sent_at     # => 2016-04-22 21:30:36 +0200
 # message.text        # => 'Hello, bot!'
-future_events = Event.where('start < ?', Time.now).reverse
-@top_events = future_events.sort_by(&:rsvps_count).last(10).reverse.map { |e| [e.name, e.start, e.location] }
+future_events = Event.where('start > ?', Date.today)
+@top_events = future_events.sort_by(&:rsvps_count).last(10).reverse.pluck(:name, :start, :location)
 
 
 Bot.on :message do |message|
