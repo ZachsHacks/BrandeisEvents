384defaaed3bc037549a5935e5f5bbb21cdeba02
Zachary Weiss <coolshale@gmail.com>
2017-05-07 11:59:55 -0400

    fixed bot more

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -7,14 +7,14 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN_Fac
 # message.sent_at     # => 2016-04-22 21:30:36 +0200
 # message.text        # => 'Hello, bot!'
 future_events = Event.where('start > ?', Date.today)
-@top_events = future_events.sort_by(&:rsvps_count).last(10).reverse.pluck(:name, :start, :location)
+@top_events = future_events.sort_by(&:rsvps_count).last(10).reverse#.pluck(:name, :start, :location)
 
 
 Bot.on :message do |message|
   Bot.deliver({
     recipient: message.sender,
     message: {
-      text: "The top upcoming event is #{@top_events[0].name}. It can be found at http://CampusNow.herokuapp.com/events/#{@top_events[0].id}. We also recomend going to #{@top_events[1].name}. It can be found at http://CampusNow.herokuapp.com/events/#{@top_events[1].id}. "
+      text: "The top upcoming event is #{@top_events.first.name}. It can be found at http://CampusNow.herokuapp.com/events/#{@top_events.first.id}. We also recomend going to #{@top_events.second.name}. It can be found at http://CampusNow.herokuapp.com/events/#{@top_events.second.id}. "
     }
   }, access_token: ENV["ACCESS_TOKEN_Facebook"])
 end
