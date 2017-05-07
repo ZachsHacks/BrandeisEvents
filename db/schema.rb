e653a77e59651920b81e11f27ef619c71adf07c0
Zachary Weiss <coolshale@gmail.com>
2017-05-06 22:53:27 -0400

    removed useless user columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426201812) do
+ActiveRecord::Schema.define(version: 20170507025253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,11 +102,6 @@ ActiveRecord::Schema.define(version: 20170426201812) do
     t.text     "bio"
     t.boolean  "can_host",   default: false
     t.boolean  "is_admin",   default: false
-    t.string   "phone"
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip_code"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
     t.index ["provider"], name: "index_users_on_provider", using: :btree
     t.index ["uid"], name: "index_users_on_uid", using: :btree
