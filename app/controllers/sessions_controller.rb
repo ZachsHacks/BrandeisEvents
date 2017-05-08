f03663b7ff13cc9b263b1ba2fa05bd5303ab2942
Ari Carr <acarr@brandeis.edu>
2017-05-07 23:24:06 -0400

    Slight calendar fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,15 @@
 class SessionsController < ApplicationController
   skip_before_action :store_location
 
-	def login_local
-		session[:user_id] = params["user_id"]
-		@user = User.find(params["user_id"])
-		redirect_back_or @user
-	end
+  def login_local
+	session[:user_id] = params["user_id"]
+	@user = User.find(params["user_id"])
+	redirect_back_or @user
+  end
 
   def create
     begin
       redirect_to root_path
-    #   @user = User.from_omniauth(request.env['omniauth.auth'])
-    #   session[:user_id] = @user.id
-    #   flash[:success] = "Welcome, #{@user.name}!"
-    #   redirect_back_or @user
     rescue
       flash[:warning] = 'There was an error while trying to authenticate you...'
       redirect_to root_path
