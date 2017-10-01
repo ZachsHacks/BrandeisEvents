19304d5e7e7adcf7102a5f52e8bb588b80c008a5
Ari Carr <acarr@brandeis.edu>
2017-10-01 14:23:06 -0400

    Removed recommendations tab

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,11 +1,13 @@
 module DashboardHelper
 
     def tab_titles
-		['RSVP\'d events', 'Interested Events', 'Events For You', 'Past Events', 'Your Account']
+		# ['RSVP\'d events', 'Interested Events', 'Recommended Events', 'Past Events', 'Your Account']
+        ['RSVP\'d events', 'Interested Events', 'Past Events', 'Your Account']
 	end
 
 	def section_titles
-		['Events you\'re going to', 'Events you\'re interested in', 'Recommended events for you', 'Past events', current_user.name]
+		# ['Events you\'re going to', 'Events you\'re interested in', 'Recommended events for you', 'Past events', current_user.name]
+        ['Events you\'re going to', 'Events you\'re interested in', 'Past events', current_user.name]
 	end
 
 end
