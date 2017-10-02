4abc87441e8612f98e3e9ad6ccdd72e2b0a0d44f
Ari Carr <acarr@brandeis.edu>
2017-10-02 13:49:36 -0400

    Send email from brandeisevents email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'brandeisevents@gmail.com'
+  default from: 'info@brandeisevents.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ config.action_mailer.raise_delivery_errors = true
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
+  domain: "brandeisevents.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
