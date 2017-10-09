db86bfc755ebd9f1f60a2a5e1b2811268282855a
Aaron Gold <aharonhillel@brandeis.edu>
2017-10-08 22:53:00 -0400

    added check for all events on home page to be in the future

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,7 +40,7 @@ class EventsController < ApplicationController
 	end
 
 	def home
-		all_events = Event.all
+		all_events = Event.where("start > ?", Time.now)
 		@items = all_events.pluck(:name)
 		@top_events = all_events.sort_by(&:rsvps_count).last(4).reverse
 		@locations = Location.all.pluck(:name) # grab_locations
