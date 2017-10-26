0cff2a90fe849b093896e50d8bce0cf04d3f2be5
Ari Carr <acarr@brandeis.edu>
2017-10-26 16:27:30 -0400

    Updated rails_admin view

--- config/initializers/rails_admin/user.rb
+++ config/initializers/rails_admin/user.rb
@@ -2,7 +2,7 @@ if User.table_exists?
   RailsAdmin.config User do
     list do
       # simply adding fields by their names (order will be maintained)
-      include_fields :id, :uid, :first_name, :last_name, :phone, :survey_sent, :events, :tags, :created_at, :updated_at, :can_host, :is_admin, :calendar_hash, :rsvps, :interests
+      include_fields :id, :uid, :first_name, :last_name, :survey_sent, :events, :tags, :created_at, :updated_at, :can_host, :is_admin, :calendar_hash, :rsvps, :interests
     end
   end
 end
