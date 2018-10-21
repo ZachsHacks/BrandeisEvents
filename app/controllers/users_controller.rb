65e32c8e19236c9c5f6f0902e30c2acf612da48f
Ari Carr <acarr@brandeis.edu>
2018-10-21 04:13:09 -0400

    More consequences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :logged_in?
 	skip_before_action :verify_authenticity_token
 
-  before_action :set_rsvps, only: [:show]
+  before_action :set_rsvps, only: [:show, :get_events]
 
   def get_events
     @active_tab = params[:index].to_i
